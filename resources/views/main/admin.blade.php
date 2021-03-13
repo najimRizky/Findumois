759e89e6430488161a03744a437260602405f5fe
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-03-13 12:47:40 +0700

    Ilangin Home Admin

--- resources/views/main/admin.blade.php
+++ resources/views/main/admin.blade.php
@@ -37,25 +37,22 @@
         }
     </style>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{asset('css')}}/app.css">
 
 </head>
 <body >
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Admin</a>
+            <a class="navbar-brand" href="/admin">Findumois (Admin)</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
             
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/admin/viewmenu">Edit Menu</a>
-                        </li>
-                        <li class="nav-item">
+                        {{-- <li class="nav-item">
                             <a class="nav-link" href="admin/profile" >Profile</a>
-                        </li>
+                        </li> --}}
                     </ul>
                     <a class="nav-link" style="color: black" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
