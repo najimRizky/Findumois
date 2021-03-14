93af8ffab186df90737a05d59c87dadfe1373f59
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-03-14 15:38:13 +0700

    Responsive v_home

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -15,15 +15,15 @@
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ul>
-            <div class="carousel-inner" style="height: 500px !important">
+            <div class="carousel-inner" style="height: auto !important">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="{{asset('Gambar')}}/Carousel1.png" alt="First slide">
+                    <img class="d-block " style="position: center; width: 100%" src="{{asset('Gambar')}}/Carousel1.png" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{asset('Gambar')}}/Carousel2.png" alt="Second slide">
+                    <img class="d-block " style="position: center; width: 100%" src="{{asset('Gambar')}}/Carousel2.png" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{asset('Gambar')}}/Carousel3.png" alt="Third slide">
+                    <img class="d-block " style="position: center; width: 100%" src="{{asset('Gambar')}}/Carousel3.png" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -37,8 +37,10 @@
         </div>
         <div class="jumbotron" style="background-color: #E1D3D3" data-aos="zoom-in">
             <div class="container d-flex justify-content-center">
-                <h1 class="display-4">SANS <br> DANGER <br> POUR LE <br> PORTEFEUILLE</h1><br/>
-                <p class="lead">Restoran pertama dengan nuansa Perancis <br> yang menyediakan berbagai menu <br>berkelas namun tetap<br> aman untuk dompet</p>
+                <div class="row justfy-content-center">
+                    <h1 class="display-5">SANS <br> DANGER <br> POUR LE <br> PORTEFEUILLE</h1><br/>
+                    <p class="lead">Restoran pertama dengan nuansa Perancis <br> yang menyediakan berbagai menu <br>berkelas namun tetap<br> aman untuk dompet</p>
+                </div>
             </div>
         </div>
         <div class="container" style="background-color: #C98787">
@@ -48,28 +50,30 @@
                     <a class="display-4" style="font-size: 20px">Menu</a>
                     <h2 class="display-4" style="font-size: 20px; color: #C98787">asdasd</h2>
                     <div style="display: flex" class="d-flex justify-content-center">
-                        <div class="card" style="width: 18rem; margin-right: 30px" data-aos="zoom-in">
-                            <img class="card-img-top" src="{{asset('Gambar')}}/miegoreng.jpg" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Mie Goreng</h5>
-                                <p class="card-text">dari mie yang dibeli dari minimarket terdekat.</p>
-                                <a href="/menu" class="btn btn-primary">Go Menu</a>
+                        <div class="row justify-content-center">
+                            <div class="card mt-2 mx-2" style="width: 18rem; " data-aos="zoom-in">
+                                <img class="card-img-top" src="{{asset('Gambar')}}/miegoreng.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Mie Goreng</h5>
+                                    <p class="card-text">dari mie yang dibeli dari minimarket terdekat.</p>
+                                    <a href="/menu" class="btn btn-primary">Go Menu</a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="card" style="width: 18rem; margin-right: 30px" data-aos="zoom-in">
-                            <img class="card-img-top" src="{{asset('Gambar')}}/nasigoreng.jpg" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Nasi Goreng</h5>
-                                <p class="card-text">nasi yang digoreng dengan minyak yang sedikit agar rendah kalori dan sehat untuk lambung sobat misqueen.</p>
-                                <a href="/menu" class="btn btn-primary">Go Menu</a>
+                            <div class="card mt-2 mx-2" style="width: 18rem;" data-aos="zoom-in">
+                                <img class="card-img-top" src="{{asset('Gambar')}}/nasigoreng.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nasi Goreng</h5>
+                                    <p class="card-text">nasi yang digoreng dengan minyak yang sedikit agar rendah kalori dan sehat untuk lambung sobat misqueen.</p>
+                                    <a href="/menu" class="btn btn-primary">Go Menu</a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="card" style="width: 18rem;" data-aos="zoom-in">
-                            <img class="card-img-top" src="{{asset('Gambar')}}/potato-chips-cure-cramps.jpg" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Kripik Kentang</h5>
-                                <p class="card-text">kentang yang ditanam sendiri dan digoreng dengan suhu yang stabil hingga garing.</p>
-                                <a href="/menu" class="btn btn-primary">Go Menu</a>
+                            <div class="card mt-2 mx-2" style="width: 18rem;" data-aos="zoom-in">
+                                <img class="card-img-top" src="{{asset('Gambar')}}/potato-chips-cure-cramps.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Kripik Kentang</h5>
+                                    <p class="card-text">kentang yang ditanam sendiri dan digoreng dengan suhu yang stabil hingga garing.</p>
+                                    <a href="/menu" class="btn btn-primary">Go Menu</a>
+                                </div>
                             </div>
                         </div>
                     </div>
